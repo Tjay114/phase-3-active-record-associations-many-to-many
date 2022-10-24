e103204335c1a51129293e110dbc1c3cb5c8ab1c
Terormumuli <terahmumuli@gmail.com>
2022-10-24 12:26:01 +0300

    Active Record Associaions Many to Many

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_024659) do
-
-  create_table "games", force: :cascade do |t|
-    t.string "title"
-    t.string "genre"
-    t.string "platform"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "score"
-    t.string "comment"
-    t.integer "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
